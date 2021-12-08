afc61e508d6223a668ac3ddc62039fd469438472
Peter Arzt <peter.arzt@stud.tu-darmstadt.de>
2021-12-08 13:22:46 +0100

    experiments with holoview

--- visualization/main.py
+++ visualization/main.py
@@ -1,7 +1,21 @@
-
-
-
-
+import numpy as np
+import holoviews as hv
+from holoviews import opts
+hv.extension('bokeh')
 
 if __name__ == '__main__':
     print('Hello world')
+
+    def random_dataset(x, y):
+        return hv.Dataset((range(x), range(y), np.random.rand(x, y)), ['x', 'y'], 'test')
+
+
+    dictonary = {hour: hv.Image(random_dataset(10, 10), ['x', 'y']) for hour in range(10)}
+    dictonary2 = {hour: hv.Image(random_dataset(10, 10), ['x', 'y']) for hour in range(10)}
+    print(dictonary)
+    holomap = hv.HoloMap(dictonary, kdims='Hour')
+    holomap2 = hv.HoloMap(dictonary, kdims='Hour')
+    layout = holomap + holomap2
+
+    # show(hv.render(holomap))
+    hv.renderer('bokeh').save(layout, 'out', fmt='widgets')
