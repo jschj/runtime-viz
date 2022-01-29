f88b6360046f22137cf386da507bc5b3455d5c9b
Peter Arzt <peter.arzt@stud.tu-darmstadt.de>
2022-01-29 13:21:12 +0100

    use argparse for command line arguments

--- visualization/main.py
+++ visualization/main.py
@@ -1,28 +1,33 @@
 import os
 import sys
 import time
+import argparse
 
 import input
 import time_information
 import visualization
 
 if __name__ == '__main__':
-    start_wclock = time.time()  # for performance measurement
-    if len(sys.argv) != 2:
-        print(f"Usage: {os.path.basename(__file__)} <buffer file>")
-        exit(255)
+    parser = argparse.ArgumentParser(description='Interactive visualiazation of CUDA memory accesses in GPU global '
+                                                 'memory. ')
 
-    buffer_filepath = sys.argv[1]
+    parser.add_argument("buffers_filepath", metavar="\"filepath to buffers.json\"", type=str)
+
+    args = parser.parse_args()
+    print(args.buffers_filepath)
+
+    start_wclock = time.time()  # for performance measurement
 
     # read input
-    buffers, access_filepaths = input.init_buffers_file(buffer_filepath)
+    buffers, access_filepaths = input.init_buffers_file(args.buffers_filepath)
     earliest_access_time = min([b.first_access_time for _, b in buffers.items()])
     latest_access_time = max([b.last_access_time for _, b in buffers.items()])
     ti = time_information.TimeInformation(earliest_access_time, latest_access_time)
     for _, b in buffers.items():
         b.initialize_heatmap(ti)
 
-    histogram = input.process_accesses(buffers, os.path.join(os.path.dirname(buffer_filepath), access_filepaths[0]), ti)
+    histogram = input.process_accesses(buffers,
+                                       os.path.join(os.path.dirname(args.buffers_filepath), access_filepaths[0]), ti)
 
     print(f"Setup took {time.time() - start_wclock:.2f} seconds. Showing visualization window...")
 
