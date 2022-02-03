Fix off-by-one-errors when calculating the number of timesteps and for the colorbar ticks

visualization/buffer.py
@@ -48,7 +48,7 @@ class Buffer:
 
         # create buffer for heatmap frames
         self.ti = ti
-        self.heatmap_frames = np.zeros(shape=(ti.timestep_count + 1, self.hm_width, self.hm_height))
+        self.heatmap_frames = np.zeros(shape=(ti.timestep_count, self.hm_width, self.hm_height))
 
     def add_access(self, timeframe_index: int, index: int):
         self.has_access = True

visualization/color_legend.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 class ColorLegend:
+    MAXIMUM_TICKS = 4
+
     def __init__(self, axis):
         self.axis = axis
         self.cbar = mpl.pyplot.colorbar(
@@ -16,15 +18,19 @@ class ColorLegend:
 
     def update_legend(self, cmap, clim):
         _, highest = clim
+        number_of_colors = int(highest) + 1
 
         # apply new colormap
-        self.cbar.update_normal(mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(0, highest + 1, clip=False), cmap=cmap))
+        self.cbar.update_normal(
+            mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(0, number_of_colors, clip=True), cmap=cmap))
 
         # draw ticks and numbers
-        number_of_ticks = max(2, min(4, highest))
-        ticks = np.linspace(0, highest + 1, num=int(number_of_ticks), endpoint=False)
-        if ticks[number_of_ticks - 1] != highest:
-            ticks = np.append(ticks, highest)
-        tick_locs = (ticks + 0.0)
+        ticks = np.linspace(0, highest, num=number_of_colors, endpoint=True)
+
+        # reduce the number of ticks
+        while ticks.size > ColorLegend.MAXIMUM_TICKS:
+            ticks = np.delete(ticks, np.arange(1, ticks.size - 1, 2))
+
+        tick_locs = (ticks + 0.5)  # shift ticks to the middle of the color
         self.cbar.set_ticks(tick_locs)
         self.cbar.set_ticklabels([int(x) for x in ticks])

visualization/heatmap.py
@@ -1,3 +1,4 @@
+import math
 from typing import Tuple
 
 import matplotlib.pyplot as plt
@@ -16,7 +17,9 @@ class Heatmap:
         :param ax: matplotlib axis to display the heatmap on
         """
         self.buffer = b
-        self.prefix_sums = self.buffer.heatmap_frames
+
+        # add a frame with only zeros in order to visualize first time step as well
+        self.prefix_sums = np.append(np.zeros(shape=(1, b.hm_width, b.hm_height)), self.buffer.heatmap_frames, 0)
 
         # devide every heatmap entry by downsampling factor to display average access count
         # this is only relevant if downsampling is active,
@@ -63,8 +66,8 @@ class Heatmap:
         b = b - self.buffer.ti.start_time
 
         # map timepoints to corresponding timestep (might not be necessary)
-        a = a // self.buffer.ti.timestep_size
-        b = b // self.buffer.ti.timestep_size
+        a = math.floor(a / self.buffer.ti.timestep_size)
+        b = math.floor(b / self.buffer.ti.timestep_size)
 
         diff = self.prefix_sums[b] - self.prefix_sums[a]
         return diff
@@ -74,4 +77,4 @@ class Heatmap:
         self.im.set_data(self.calc_frame(timerange=timerange))
 
     def get_maximum(self):
-        return np.max(self.prefix_sums)
+        return np.ceil(np.max(self.prefix_sums))

visualization/input.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os.path
 import struct
 import zlib
@@ -43,6 +44,7 @@ def process_accesses(buffers: buffer.BufferCollection,
                      ti: time_information.TimeInformation,
                      histogram: np.ndarray):
     """
+    :param histogram:
     :param buffers: BufferCollection with buffers. This function will register accesses to the corresponding buffer.
     :param access_filepath: Filepath to zlib-compressed binary file containing access information.
     :param ti: Time Info
@@ -66,7 +68,7 @@ def process_accesses(buffers: buffer.BufferCollection,
             for bufferid, timestamp, index in struct.iter_unpack("<BQL", decompressed_data):
                 # calculate frame index (time domain)
                 relative_tp = timestamp - ti.start_time
-                frame_index = relative_tp // ti.timestep_size
+                frame_index = math.floor(relative_tp / ti.timestep_size)
 
                 # update histogram
                 histogram[frame_index] += 1

visualization/main.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
                         metavar="regex",
                         help="Only buffers with a name matching this regex will be visualized.")
     parser.add_argument("-n", "--buffer-negative-regex",
-                        default="(?!x)x",  # guaranteed never to many anything
+                        default="(?!x)x",  # guaranteed never to match anything
                         type=str,
                         required=False,
                         dest="buffer_negative_regex",
@@ -52,7 +52,7 @@ if __name__ == '__main__':
         b.initialize_heatmap(ti)
 
     # initialize histogram
-    histogram = np.zeros(shape=(ti.timestep_count + 1,))
+    histogram = np.zeros(shape=(ti.timestep_count,))
 
     # read access files ("xy.accesses.bin") which match regex
     for filename in access_filepaths:

visualization/time_information.py
@@ -1,3 +1,7 @@
+import math
+
+import numpy as np
+
 TIME_RES = 50
 
 
@@ -6,9 +10,19 @@ class TimeInformation:
         self.start_time = start_time
         self.end_time = end_time
         self.duration = self.end_time - self.start_time
-        self.start_time = self.start_time - self.duration // 20
-        self.end_time = self.end_time + self.duration // 20
-        self.duration = self.end_time - self.start_time
 
         self.timestep_size = max(self.duration // TIME_RES, 1)
-        self.timestep_count = self.duration // self.timestep_size
+        self.timestep_count = math.floor(self.duration / self.timestep_size + 1.0)
+
+        print(f"Time information: start: {self.start_time} "
+              f"end: {self.end_time}, "
+              f"duration: {self.duration}, "
+              f"timestep size: {self.timestep_size}, "
+              f"timestep count: {self.timestep_count}")
+
+    def get_timesteps(self):
+        end = self.start_time + self.timestep_size * self.timestep_count
+        timesteps = np.arange(self.start_time, end, self.timestep_size, )
+        assert (len(timesteps) == self.timestep_count)
+
+        return timesteps

visualization/visualization.py
@@ -50,7 +50,7 @@ class Visualization:
             self.apply_colormap(highest=highest)
 
     def apply_colormap(self, highest: int):
-        # get discrete colormap
+        # get a discrete colormap with highest+1 colors as we need a color for zero accesses
         self.cmap = mpl.pyplot.get_cmap('copper', highest + 1)
         self.clim = (0, highest)
 
@@ -95,7 +95,7 @@ class Visualization:
 
         # Create overview plot that ranges over all columns
         overview = mpl.pyplot.subplot(rows, 1, rows)
-        mpl.pyplot.plot(np.arange(self.ti.start_time, self.ti.end_time, self.ti.timestep_size), self.histogram)
+        mpl.pyplot.bar(self.ti.get_timesteps(), self.histogram, width=self.ti.timestep_size)
         mpl.pyplot.xlim(self.ti.start_time - self.ti.duration // 20, self.ti.end_time + self.ti.duration // 20)
         mpl.pyplot.title("Access histogram")
         mpl.pyplot.xlabel("Time (ns)")